9f6001386e85c3443cbd3be4c562a26df5f38b29
ein <victorpanayotov@gmail.com>
2018-12-09 18:19:55 +0000

    Introduced object input to issue components

---
+++ src/app/issue.ts
@@ -0,0 +1,9 @@
+import { User } from "./user";
+
+export class Issue {
+    constructor(
+        public title : string,
+        public description : string,
+        public user : User
+    ){}
+}
---
+++ src/app/user.ts
@@ -0,0 +1,7 @@
+export class User {
+    constructor(
+        public name : string,
+        public reputation : number,
+        public institute : string
+    ){}
+}
