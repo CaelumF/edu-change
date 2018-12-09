51f6d3ab9645b35e0abf1a8449afa4269f3f29d0
ein <victorpanayotov@gmail.com>
2018-12-09 21:28:29 +0000

    Issue UI rework

--- src/app/issue/issue.component.ts
+++ src/app/issue/issue.component.ts
@@ -7,14 +7,16 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class IssueComponent implements OnInit {
 
-  @Input() author : String;
   @Input() title : String;
+  @Input() author : String;
+  @Input() institute : String;
   @Input() description : String;
 
   constructor() {
     //Mockup
     this.author = "Victor Panayotov";
-    this.title = "Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|";
+    this.institute = "University College Cork"
+    this.title = "Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|";
     this.description = "Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|"; // Use rich text?
    }
 
