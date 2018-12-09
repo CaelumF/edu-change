2bb6cc72797bfc3a53df613a0d9e2e62d878d41b
ein <victorpanayotov@gmail.com>
2018-12-09 20:20:21 +0000

    Issues now display a title, preview of the description and author Issues store a title, description and author as inputs
    Added normalize.css to the project but not using it
    Colored for debugging

--- src/app/issue/issue.component.ts
+++ src/app/issue/issue.component.ts
@@ -14,8 +14,8 @@ export class IssueComponent implements OnInit {
   constructor() {
     //Mockup
     this.author = "Victor Panayotov";
-    this.title = "Bathrooms are in poor shape";
-    this.description = "Recently, I've noticed that the bathrooms in the university are in incredibly poor shape"; // Use rich text?
+    this.title = "Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|Bathrooms are in poor shape|";
+    this.description = "Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|Recently, I've noticed that the bathrooms in the university are in incredibly poor shape|"; // Use rich text?
    }
 
   ngOnInit() {
