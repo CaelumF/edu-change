c09cae570a950ed494272e84023fe4910b435ed5
ein <victorpanayotov@gmail.com>
2018-12-14 20:53:55 +0000

    Improved UI and added response to Issue

--- src/app/issue.ts
+++ src/app/issue.ts
@@ -1,10 +1,12 @@
 import { User } from './user';
+import { Resolution } from './resolution';
 
 export class Issue {
     constructor(
         public id: number,
         public title: string,
         public description: string,
+        public resolution: Resolution,
         public user: User
     ) {}
 }
