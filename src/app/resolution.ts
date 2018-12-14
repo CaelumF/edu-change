6ccd8151c761a6276da67e325e09b45a00b5faa6
ein <victorpanayotov@gmail.com>
2018-12-14 23:56:24 +0000

    Accidentally commented too much out

--- src/app/resolution.ts
+++ src/app/resolution.ts
@@ -4,9 +4,9 @@ export class Resolution {
     constructor (
         // public raw: Resolution,
         public id: number, // = -1,
-        public title: string, // = null,
-        public description: string, // = null,
-        public user: User, // = null
+        public title: string = null,
+        public description: string = null,
+        public user: User = null
     ) {
         // this.id = this.raw.id;
         // this.title = this.raw.title;
