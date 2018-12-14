bd18fad2739f24fbe391212a92a9bde42f795395
ein <victorpanayotov@gmail.com>
2018-12-14 23:54:29 +0000

    Trying to add JSON support in a really bad way, left where I got commented out

--- src/app/issue.service.ts
+++ src/app/issue.service.ts
@@ -5,13 +5,24 @@ import { Observable, of } from 'rxjs';
 import { ISSUES } from 'src/assets/issues';
 import { map } from 'rxjs/operators';
 
+import { } from '../assets/issues';
+
 @Injectable({
   providedIn: 'root'
 })
 export class IssueService {
   constructor() { }
-
+  request: XMLHttpRequest = new XMLHttpRequest;
   getIssues (): Observable<Issue[]> {
+    // const ISSUES: Issue[] = [];
+    // this.request.open('GET', '../../assets/issues.json', false);
+    // this.request.send(null);
+    // console.log(JSON.parse(this.request.responseText));
+    // const raw: [] = JSON.parse(this.request.responseText);
+    // raw.forEach(element => {
+    //   console.log(element.valueOf("description"));
+    // });
+
     return of(ISSUES);
   }
   getIssue (id: number) {
