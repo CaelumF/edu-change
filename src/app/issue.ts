f3682d4036acd79c74423f03b4873c1a24966e20
ein <victorpanayotov@gmail.com>
2019-01-12 04:14:36 +0000

    Changed issue page to use mat-card for continuity

--- src/app/issue.ts
+++ src/app/issue.ts
@@ -2,17 +2,10 @@ import DocumentReference = firebase.firestore.DocumentReference;
 
 export class Issue {
     constructor(
-        // public raw: Issue,
-        public id: number, // = -1,
-        public title: string, // = null,
-        public description: string, // = null,
+        public id: number,
+        public title: string,
+        public description: string,
         public resolutions: DocumentReference[] = null,
         public user: DocumentReference
-    ) {
-        // this.id = this.raw.id;
-        // this.title = this.raw.title;
-        // this.description = this.raw.description;
-        // this.resolution = new Resolution(this.raw.resolution);
-        // this.user = new User(this.raw.user);
-    }
+    ) { }
 }
